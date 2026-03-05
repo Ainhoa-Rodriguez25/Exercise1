6404fc8af5dcd2e5fd1f0d4d4f45eeb4cc50a1bd
Ainhoa-Rodriguez <ainhoa.rodriguez@goumh.umh.es>
2026-03-05 19:56:01 +0100

    Create PalindromeController

---
+++ p1-springboot-hello-world-app/src/main/java/demoapp/controller/PalindromeController.java
@@ -0,0 +1,35 @@
+package demoapp.controller;
+
+import demoapp.service.PalindromeService;
+import jdk.internal.classfile.impl.BufferedCodeBuilder;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+
+import javax.validation.Valid;
+
+@Controller
+public class PalindromeController {
+
+    @Autowired
+    private PalindromeService service;
+
+    @GetMapping("/palindrome")
+    public String showForm(PalindromeData palindromeData) {
+        return "palindromeForm";
+    }
+
+    @PostMapping("/palindrome")
+    public String checkPalindrome(@Valid PalindromeData palindromeData, BindingResult result, Model model) {
+        if (result.hasErrors()) {
+            return "palindromeForm";
+        }
+
+        boolean isPalindrome = service.isPalindrome(palindromeData.getWord());
+        model.addAttribute("result", isPalindrome);
+
+        return "palindromeResult";
+    }
+}
