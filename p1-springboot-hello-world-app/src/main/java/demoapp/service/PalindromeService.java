595261c27305d4020c6b8e17a1d988efa1af4729
Ainhoa-Rodriguez <ainhoa.rodriguez@goumh.umh.es>
2026-03-05 19:31:52 +0100

    Second update 2 PalindromeService.jar

--- p1-springboot-hello-world-app/src/main/java/demoapp/service/PalindromeService.java
+++ p1-springboot-hello-world-app/src/main/java/demoapp/service/PalindromeService.java
@@ -4,15 +4,15 @@ import org.springframework.stereotype.Service;
 
 @Service
 public class PalindromeService {
-    public Boolean isPalindrome (String word) {
+    public boolean isPalindrome (String word) {
         // Palindrome: word that reads the same backwards and fowards
         if (word == null) {
             return false;
         }
 
         String cleaned = word.replaceAll("\\s+", "").toLowerCase();
-        String reversed = new StringBuilder(word).reverse().toString();
+        String reversed = new StringBuilder(cleaned).reverse().toString();
 
-        return cleaned.equalsIgnoreCase(reversed);
+        return cleaned.equals(reversed);
     }
 }
