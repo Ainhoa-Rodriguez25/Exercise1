56bdd51d9ff4c36dd785056f7acbfbf1b286cf56
Ainhoa-Rodriguez <ainhoa.rodriguez@goumh.umh.es>
2026-03-05 19:39:16 +0100

    Create PalindromeData form model

---
+++ p1-springboot-hello-world-app/src/main/java/demoapp/controller/PalindromeData.java
@@ -0,0 +1,18 @@
+package demoapp.controller;
+
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Size;
+
+public class PalindromeData {
+    @NotBlank // It's used to check that a char sequence is not null and that its length is greater than 0
+    @Size(min = 1, max = 30)
+    private String word;
+
+    public String getWord() {
+        return word;
+    }
+
+    public void setWord(String word) {
+        this.word = word;
+    }
+}
