a1d2270eb0d6e821e2b8c5ffeb0db1fd185be977
Ainhoa-Rodriguez <ainhoa.rodriguez@goumh.umh.es>
2026-03-05 20:16:38 +0100

    Update PalindromeData.jar

--- p1-springboot-hello-world-app/src/main/java/demoapp/controller/PalindromeData.java
+++ p1-springboot-hello-world-app/src/main/java/demoapp/controller/PalindromeData.java
@@ -1,9 +1,11 @@
 package demoapp.controller;
 
+import javax.validation.Valid;
 import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.Size;
 
 public class PalindromeData {
+    @Valid
     @NotBlank // It's used to check that a char sequence is not null and that its length is greater than 0
     @Size(min = 1, max = 30)
     private String word;
