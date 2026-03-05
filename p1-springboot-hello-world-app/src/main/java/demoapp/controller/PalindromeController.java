93c78feffb087c1ca938c7d90959a6293df92042
Ainhoa-Rodriguez <ainhoa.rodriguez@goumh.umh.es>
2026-03-05 20:01:39 +0100

    Update PalindromeController.jar

--- p1-springboot-hello-world-app/src/main/java/demoapp/controller/PalindromeController.java
+++ p1-springboot-hello-world-app/src/main/java/demoapp/controller/PalindromeController.java
@@ -4,6 +4,7 @@ import demoapp.service.PalindromeService;
 import jdk.internal.classfile.impl.BufferedCodeBuilder;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
