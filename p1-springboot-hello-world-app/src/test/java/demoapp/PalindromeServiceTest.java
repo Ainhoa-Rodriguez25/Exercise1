b10f01e2384abb10ddbf394b951907591d36ef27
Ainhoa-Rodriguez <ainhoa.rodriguez@goumh.umh.es>
2026-03-06 13:03:00 +0100

    Create and improve PalindromeService tests

---
+++ p1-springboot-hello-world-app/src/test/java/demoapp/PalindromeServiceTest.java
@@ -0,0 +1,41 @@
+package demoapp;
+
+import demoapp.service.PalindromeService;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+public class PalindromeServiceTest {
+
+    private PalindromeService service;
+
+    @BeforeEach
+    void setUp() {
+        service = new PalindromeService();
+    }
+
+    // Check that the word is a palindrome
+    @Test
+    void returnTrueWhenWordIsPalindrome() {
+        assertTrue(service.isPalindrome("radar"));
+    }
+
+    // Test to check if the word is not a palindrome
+    @Test
+    void returnFalseWhenWordIsNotPalindrome() {
+        assertFalse(service.isPalindrome("hello"));
+    }
+
+    // Test to verify that the programe ignores spaces
+    @Test
+    void ignoreSpacesAndCase() {
+        assertTrue(service.isPalindrome("Anita lava la tina"));
+    }
+
+    // Test that should return false if the word is null
+    @Test
+    void returnFalseWhenWordIsNull() {
+        assertFalse(service.isPalindrome(null));
+    }
+}
