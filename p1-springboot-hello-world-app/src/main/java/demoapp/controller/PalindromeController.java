1f396e3728c1efebdda9323a9816b573adbe0dac
Ainhoa-Rodriguez <ainhoa.rodriguez@goumh.umh.es>
2026-03-06 13:26:20 +0100

    Create PalindromeController tests
    To verify that PalindromeControllerTest worked, the PalindromeController file had to be modified

--- p1-springboot-hello-world-app/src/main/java/demoapp/controller/PalindromeController.java
+++ p1-springboot-hello-world-app/src/main/java/demoapp/controller/PalindromeController.java
@@ -1,7 +1,6 @@
 package demoapp.controller;
 
 import demoapp.service.PalindromeService;
-import jdk.internal.classfile.impl.BufferedCodeBuilder;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
---
+++ p1-springboot-hello-world-app/src/test/java/demoapp/PalindromeControllerTest.java
@@ -0,0 +1,42 @@
+package demoapp;
+
+import demoapp.controller.PalindromeController;
+import demoapp.service.PalindromeService;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.test.web.servlet.MockMvc;
+
+import static org.mockito.Mockito.when;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+
+@WebMvcTest(PalindromeController.class)
+public class PalindromeControllerTest {
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @MockBean
+    private PalindromeService service;
+
+    @Test
+    void loadPalindromeForm() throws Exception {
+        mockMvc.perform(get("/palindrome"))
+                .andExpect(status().isOk())
+                .andExpect(view().name("palindromeForm"));
+    }
+
+    @Test
+    void returnResultWhenPostingValidWord() throws Exception {
+        when(service.isPalindrome("radar")).thenReturn(true);
+
+        mockMvc.perform(post("/palindrome")
+                .param("word", "radar"))
+                .andExpect(status().isOk())
+                .andExpect(view().name("palindromeResult"))
+                .andExpect(model().attributeExists("result"));
+    }
+}
