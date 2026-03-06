2214bbc710f6142ea8d9c48dd2b2c834893538fb
Ainhoa-Rodriguez <ainhoa.rodriguez@goumh.umh.es>
2026-03-06 14:34:55 +0100

    Create EvenNumberService tests

---
+++ p1-springboot-hello-world-app/src/test/java/demoapp/service/EvenNumberServiceTest.java
@@ -0,0 +1,22 @@
+package demoapp.service;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+public class EvenNumberServiceTest {
+    private EvenNumberService service;
+
+    @BeforeEach
+    void setUp() {service = new EvenNumberService();}
+
+    @Test
+    void returnTrueWhenNumberisEven() {assertTrue(service.isEven(4));}
+
+    @Test
+    void returnFalseWhenNumberIsOdd() {assertFalse(service.isEven(7));}
+
+    @Test
+    void workWithNegativeNumbers() {assertTrue(service.isEven(-2));}
+}
