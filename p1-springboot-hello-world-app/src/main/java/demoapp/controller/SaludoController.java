aa1a961d77cc0646394616ab4b476c3b17db7c79
Ainhoa-Rodriguez <ainhoa.rodriguez@goumh.umh.es>
2026-03-08 18:18:45 +0100

    Update SaludoController.java

--- p1-springboot-hello-world-app/src/main/java/demoapp/controller/SaludoController.java
+++ p1-springboot-hello-world-app/src/main/java/demoapp/controller/SaludoController.java
@@ -15,10 +15,11 @@ public class SaludoController {
     @Autowired // Te permite utilizar clases que no están en este archivo sino en el archivo que se indica en la línea que sigue
     private SaludoService service;
 
+    /*
     @RequestMapping("/saludo/{nombre}")
     public @ResponseBody String saludo(@PathVariable(value="nombre") String nombre) {
         return service.saluda(nombre);
-    }
+    }*/
 
     @GetMapping("/")
     public String home() {
