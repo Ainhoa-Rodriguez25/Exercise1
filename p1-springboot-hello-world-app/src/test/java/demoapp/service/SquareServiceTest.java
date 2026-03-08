1abc512de588904ef64e7a175f9622e7055b3aec
Ainhoa-Rodriguez <ainhoa.rodriguez@goumh.umh.es>
2026-03-08 17:55:12 +0100

    Create SquareService tests

---
+++ p1-springboot-hello-world-app/src/test/java/demoapp/service/SquareServiceTest.java
@@ -0,0 +1,25 @@
+package demoapp.service;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+public class SquareServiceTest {
+    private SquareService service;
+
+    @BeforeEach
+    void setUp() {service = new SquareService();}
+
+    @Test
+    void returnTrueWhenNumber2IsSquare() {assertTrue(service.isSquare(3, 9));}
+
+    @Test
+    void returnTrueWhenNumber2IsNotSquare() {assertFalse(service.isSquare(3,8));}
+
+    @Test
+    void workWithNegativeNumbers() {assertTrue(service.isSquare(-3, 9));}
+
+    @Test
+    void workWithZero() { assertTrue(service.isSquare(0, 0));}
+}
