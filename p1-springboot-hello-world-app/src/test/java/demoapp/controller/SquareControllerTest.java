cf5762ec2e6f1d6b193118ba4850dab824f07af5
Ainhoa-Rodriguez <ainhoa.rodriguez@goumh.umh.es>
2026-03-08 17:59:55 +0100

    Create SquareController tests

---
+++ p1-springboot-hello-world-app/src/test/java/demoapp/controller/SquareControllerTest.java
@@ -0,0 +1,54 @@
+package demoapp.controller;
+
+import demoapp.service.EvenNumberService;
+import demoapp.service.SquareService;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.test.web.servlet.MockMvc;
+
+import static org.mockito.Mockito.when;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+
+@WebMvcTest(SquareController.class)
+public class SquareControllerTest {
+    @Autowired
+    private MockMvc mockMvc;
+
+    @MockBean
+    private SquareService service;
+
+    @Test
+    void loadSquareForm() throws Exception {
+        mockMvc.perform(get("/square"))
+                .andExpect(status().isOk())
+                .andExpect(view().name("squareForm"));
+    }
+
+    @Test
+    void returnResultWhenSquareIsValid() throws Exception {
+        when(service.isSquare(3, 9)).thenReturn(true);
+
+        mockMvc.perform(post("/square")
+                        .param("num1", "3")
+                        .param("num2", "9"))
+                .andExpect(status().isOk())
+                .andExpect(view().name("squareResult"))
+                .andExpect(model().attributeExists("result"));
+    }
+
+    @Test
+    void returnResultWhenSquareIsNotValid() throws Exception {
+        when(service.isSquare(3, 8)).thenReturn(true);
+
+        mockMvc.perform(post("/square")
+                        .param("num1", "3")
+                        .param("num2", "8"))
+                .andExpect(status().isOk())
+                .andExpect(view().name("squareResult"))
+                .andExpect(model().attributeExists("result"));
+    }
+}
