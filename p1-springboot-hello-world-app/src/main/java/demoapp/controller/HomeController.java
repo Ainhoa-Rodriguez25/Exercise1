d50199912b785fac1950a72a98f23d65c195cf09
Ainhoa-Rodriguez <ainhoa.rodriguez@goumh.umh.es>
2026-03-08 19:08:59 +0100

    Update HomeController.java
    This fila has been modified for the correct development of the home page

--- p1-springboot-hello-world-app/src/main/java/demoapp/controller/HomeController.java
+++ p1-springboot-hello-world-app/src/main/java/demoapp/controller/HomeController.java
@@ -1,14 +1,15 @@
 package demoapp.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class HomeController {
 
-    @RequestMapping("/")
-    public @ResponseBody String greeting() {
-        return "Hello, this is my first spring-boot aplication";
+    @GetMapping("/")
+    public String home() {
+        return "home";
     }
 }
