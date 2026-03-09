e689394ba54181ac925a703ea134e3e149ef3ae9
Ainhoa-Rodriguez <ainhoa.rodriguez@goumh.umh.es>
2026-03-09 12:39:22 +0100

    Create FactorialService.java
    This file implements the logic for the optional functionality, which will calculate the factorial of the number specified by the user

---
+++ p1-springboot-hello-world-app/src/main/java/demoapp/service/FactorialService.java
@@ -0,0 +1,21 @@
+package demoapp.service;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class FactorialService {
+    public long calculateFactorial(int number) {
+        // Number validation
+        if (number < 0) {
+            throw new IllegalArgumentException("Number must be non-negative");
+        }
+        
+        long result = 1;
+
+        for (int i = 1; i <= number; i++) {
+            result *= i;
+        }
+
+        return result;
+    }
+}
