6b7bf163294704957e8193f2fb8fd96922431d1c
Ainhoa-Rodriguez <ainhoa.rodriguez@goumh.umh.es>
2026-03-10 11:47:03 +0100

    Update SquareController.java

--- p1-springboot-hello-world-app/src/main/java/demoapp/controller/square/SquareController.java
+++ p1-springboot-hello-world-app/src/main/java/demoapp/controller/square/SquareController.java
@@ -19,18 +19,18 @@ public class SquareController {
     public String showForm(Model model) {
         model.addAttribute("squareData", new SquareData());
 
-        return "squareForm";
+        return "square/squareForm";
     }
 
     @PostMapping("/square")
     public String checkSquare(@Valid SquareData squareData, BindingResult result, Model model) {
         if (result.hasErrors()) {
-            return "squareForm";
+            return "square/squareForm";
         }
 
         boolean isSquare = service.isSquare(squareData.getNum1(), squareData.getNum2());
         model.addAttribute("result", isSquare);
 
-        return "squareResult";
+        return "square/squareResult";
     }
 }
