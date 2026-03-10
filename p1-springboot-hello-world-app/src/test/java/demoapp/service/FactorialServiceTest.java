ccada68c72f38798048fcb5a6ac9e65d9ddccb88
Ainhoa-Rodriguez <ainhoa.rodriguez@goumh.umh.es>
2026-03-10 11:06:10 +0100

    Create FactorialService tests

---
+++ p1-springboot-hello-world-app/src/test/java/demoapp/service/FactorialServiceTest.java
@@ -0,0 +1,33 @@
+package demoapp.service;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+public class FactorialServiceTest {
+    private FactorialService service;
+
+    @BeforeEach
+    void setUp() {service = new FactorialService();}
+
+    @Test
+    void returnResulFor5() {
+        assertEquals(120L, service.calculateFactorial(5));
+    }
+
+    @Test
+    void shouldReturn1For0() {
+        assertEquals(1L, service.calculateFactorial(0));
+    }
+
+    @Test
+    void shouldReturn1For1() {
+        assertEquals(1L, service.calculateFactorial(1));
+    }
+
+    @Test
+    void shouldReturnCorrectValueFor20() {
+        assertEquals(2432902008176640000L, service.calculateFactorial(20));
+    }
+}
